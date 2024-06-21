8056ee53d890d726ace986199eeed6c757e63592
INGENIO <ingeniokhalif@gmail.com>
2024-06-21 19:07:24 +0100

    send notification sms to candidate on the waitlist when a new recruitment session is planned

---
+++ src/hooks/sessions.hooks.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata";
+
+import EventEmitter from "node:events";
+import { SESSIONS } from "../utils/constants/hooks";
+import Container from "typedi";
+import WaitlistServices from "../services/waitlist.services";
+import SmsServices from "../services/sms.services";
+import CandidatesServices from "../services/candidates.services";
+import logger from "../utils/logger";
+
+const SessionsEvents = new EventEmitter();
+
+SessionsEvents.on(SESSIONS.CREATED, async (date: string) => {
+  const waitlist = Container.get(WaitlistServices);
+  const sms = Container.get(SmsServices);
+  const candidatesServices = Container.get(CandidatesServices);
+
+  try {
+    const candidates = await waitlist.getWaitlist();
+
+    candidates.forEach(async (candidate) => {
+      const cndidate = await candidatesServices.getCandidate({
+        candidateId: candidate._id.toString(),
+        raiseException: false,
+      });
+
+      //   Send new session created notification
+      if (cndidate) {
+        const { firstname, lastname, publicId, phone } = cndidate;
+        await sms.sendNewSessionCreatedSms({
+          firstname: firstname as string,
+          lastname,
+          publicId: publicId as string,
+          date,
+          phone: phone as string,
+        });
+
+        // TODO: Remove candidate from waitlist
+      }
+    });
+  } catch (error) {
+    logger.error(error);
+  }
+});
+
+export default SessionsEvents;
--- src/services/sessions.services.ts
+++ src/services/sessions.services.ts
@@ -1,13 +1,19 @@
 import { Service } from "typedi";
 import SessionsRepo from "../repositories/sessions.repository";
 import { CreateSession } from "../schemas/sessions.schemas";
+import SessionsEvents from "../hooks/sessions.hooks";
+import { SESSIONS } from "../utils/constants/hooks";
 
 @Service()
 export default class SessionsServices {
   constructor(private repository: SessionsRepo) {}
 
   async createSession({ date }: CreateSession["body"]) {
-    return await this.repository.createSession({ date });
+    const session = await this.repository.createSession({ date });
+
+    SessionsEvents.emit(SESSIONS.CREATED);
+
+    return session;
   }
 
   async getSessions() {
